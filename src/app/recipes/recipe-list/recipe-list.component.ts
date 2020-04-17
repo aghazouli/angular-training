c62d81fc9b2e5ce278e3ea133e0c7f4580cf2172
aghazouli <ghazouli.abdelhamid@gmail.com>
2020-04-17 23:24:43 +0100

    child route and add new component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -13,10 +14,16 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] ;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route});
   }
 
   // onRecipeSelected(recipe: Recipe) {
